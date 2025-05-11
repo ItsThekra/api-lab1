9912173a76942f84fdbe6af21e72d103822d1b13
Thekra Aljagthmi <thekra.aljagthmi@gmail.com>
2025-05-11 11:23:54 +0300

    fix the image problem

--- main.js
+++ main.js
@@ -1,72 +1,28 @@
-// let container = document.getElementById("container");
-// let body = document.body
-// let card = document.createElement("div")
-
-// let name_api = fetch("https://rickandmortyapi.com/api/character")
-// .then((Response) => Response.json())
-// .then((data) =>{
-//     data.results.forEach(item => {
-//     let name = document.createElement("h3");
-//     let img = document.createElement("img")
-//     img.src = item.image
-//     name.innerText = item.name; 
-
-//      card.appendChild(name);
-//      card.appendChild(img)
-//      container.appendChild(card);
-
-//     }) 
-
-//     body.appendChild(container)
-// });
-
-
-
-
-
-
-// let container = document.getElementById("container");
-// let body = document.body;
-
-// let name_api = fetch("https://rickandmortyapi.com/api/character")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     data.results.forEach(item => {
-//       let card = document.createElement("div");
-//       let name = document.createElement("h3");
-//       name.innerText = item.name;
-
-//       card.appendChild(name);
-
-//       container.appendChild(card);
-//     });
-
-//     body.appendChild(container);
-//   });
-
-
 let container = document.getElementById("container");
 let body = document.body;
 
-let name_api = fetch("https://rickandmortyapi.com/api/character")
+let api = fetch("https://rickandmortyapi.com/api/character")
   .then((response) => response.json())
   .then((data) => {
-    data.results.forEach(item => {
+      data.results.forEach(item => {
       let card = document.createElement("div");
       card.classList.add("card"); 
 
       let image = document.createElement("img");
-      image.src = item.image; // تعيين الرابط الخاص بالصورة
+      image.src = item.image; 
 
       
       let name = document.createElement("h3");
       name.innerText = item.name;
 
+      // add name and img to card:
       card.appendChild(image);
       card.appendChild(name);
 
+      // add card to container:
       container.appendChild(card);
     });
 
+       // adding container to body: 
     body.appendChild(container);
   })
